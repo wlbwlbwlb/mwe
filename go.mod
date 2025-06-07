30b136077ed7fc83fe3cc96272e956fb319bc3e2
jq <jq.go@qq.com>
2025-06-07 16:34:39 +0800

    debug

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/wlbwlbwlb/mwe
 
-go 1.23.2
+go 1.24
 
 require github.com/shopspring/decimal v1.4.0
---
+++ trans.go
@@ -0,0 +1,17 @@
+package mwe
+
+func Int(a int) *int {
+	return &a
+}
+
+func Int64(a int64) *int64 {
+	return &a
+}
+
+func String(a string) *string {
+	return &a
+}
+
+func Bool(a bool) *bool {
+	return &a
+}
